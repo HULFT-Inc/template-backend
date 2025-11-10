02fec3884bada652ee1a59901740ebc1ee6dbe18
Drew Stoneburger <astoneburger@hulftinc.com>
2025-11-10 10:19:37 -0500

    Add logback configuration and logging - 2025-11-10 10:19:37

--- src/main/java/com/example/template/HealthController.java
+++ src/main/java/com/example/template/HealthController.java
@@ -10,15 +10,20 @@ import io.micronaut.http.annotation.Get;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Controller("/health")
 @Tag(name = "Health", description = "Health check operations")
 public class HealthController {
 
+  private static final Logger LOG = LoggerFactory.getLogger(HealthController.class);
+
   @Get
   @Operation(summary = "Health check", description = "Returns the health status of the service")
   @ApiResponse(responseCode = "200", description = "Service is healthy")
   public String health() {
+    LOG.info("Health check requested");
     return "OK";
   }
 }
